2b41aabf003a48a9cd434ceb6b522136b52336d6
Benoit <benoit@example.com>
2025-10-29 21:58:59 +0100

    📦 NEW: structure hero

--- data.php
+++ data.php
@@ -3,5 +3,16 @@
 function getData(): array {
     return [
         'title' => 'Hello World',
+        'menu' => [
+            'home' => 'Accueil',
+            'why' => 'Pourquoi vendre ?',
+            'activities' => 'Secteurs d’activité',
+            'process' => 'Processus',
+        ],
+        'hero' => [
+            'title' => 'MNR Partners, votre agrégateur d’entreprises.',
+            'description' => 'Vendez votre société en toute confiance à un partenaire solide et fiable.',
+            'image' => 'https://placehold.co/600x400',
+        ],
     ];
 }
