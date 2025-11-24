732f34ac314eb94c445a983277cb2b899cb4271e
benoit <benoit.barberot@alpeshabitat.fr>
2025-11-24 21:07:33 +0100

    👌 IMPROVE: misc

---
+++ install.sh
@@ -0,0 +1,6 @@
+php bin/console flasher:install
+php bin/console tailwind:build --minify
+php bin/console cache:clear
+php bin/console cache:warmup
+chown -R www-data:www-data ./var
+chmod -R ug+wr ./var
